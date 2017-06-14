127095169d9dd825ca147135236ffcae1b1e31d9
Golars <titorens@gmail.com>
2017-06-14 17:48:34 +0300

    [+] Java mvn simple project

---
+++ src/main/java/codewarsJava/App.java
@@ -0,0 +1,13 @@
+package codewarsJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from Codewars Java!" );
+    }
+}
